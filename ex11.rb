b7501621ad52fc4ee38ba4d12e364f0b06247622
Kurt Meyerhofer <k@kcmr.io>
2017-01-16 12:36:11 -0700

    adds additional question to ex11.rb

--- ex11.rb
+++ ex11.rb
@@ -6,3 +6,12 @@ print "How much do you weight? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What year were you born? "
+year = gets.chomp
+
+if year.to_i > 2010 
+  puts "How did you get access to this computer?"
+else 
+  puts "A fine year indeed."
+end
