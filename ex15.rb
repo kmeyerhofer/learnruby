c2a0f5491ed2d4d121192c36fba43a3cad18279c
Kurt Meyerhofer <k@kcmr.io>
2017-01-18 21:08:23 -0700

    adds work from study drills for Exercise 15

--- ex15.rb
+++ ex15.rb
@@ -1,13 +1,15 @@
-filename = ARGV.first
+filename = ARGV.first # gets the filename from the initial argument
 
-txt = open(filename)
+txt = open(filename) # defines txt as the open function with filename as the argument
 
-puts "Here's your file #{filename}:"
-print txt.read
+puts "Here's your file #{filename}:" # displays text of the filename
+print txt.read # prints the read function on txt
+txt.close # closes opened file
 
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
+print "Type the filename: "
+file_again = $stdin.gets.chomp # asks user for the file name
 
-txt_again = open(file_again)
+txt_again = open(file_again) # open function with file_again as the argument
 
-print txt_again.read
+print txt_again.read # prints the read function on txt_again
+txt_again.close # closes opened file
