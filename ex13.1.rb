b23763240b7afd519f6fadae89f3c80c2ef68809
Kurt Meyerhofer <k@kcmr.io>
2017-01-18 20:29:43 -0700

    adds ex13.1.rb

---
+++ ex13.1.rb
@@ -0,0 +1,8 @@
+first, second = ARGV
+
+print "Choose your third variable: "
+third = $stdin.gets.chomp
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
