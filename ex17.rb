6b9543f062a14485d854bf5e743b9dfea45ecc03
Kurt Meyerhofer <k@kcmr.io>
2017-01-21 13:57:45 -0700

    simplifies code

--- ex17.rb
+++ ex17.rb
@@ -1,19 +1,7 @@
 from_file, to_file = ARGV
-
-puts "Copying from #(from_file} to #{to_file}"
-
-# we could do these two on one line, how?
+puts "Copying from #{from_file} to #{to_file}"
 indata = open(from_file).read
-
-puts "The input file is #{indata.length} bytes long"
-
-puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
-
 out_file = open(to_file, 'w')
 out_file.write(indata)
-
-puts "Alright, all done."
-
+puts "Completed."
 out_file.close
