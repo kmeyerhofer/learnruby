359d367e7be5185d828be8997bbd5bda7b5a2dfd
Kurt Meyerhofer <k@kcmr.io>
2017-06-25 16:42:49 -0700

    adds ex21

--- ex21.rb
+++ ex21.rb
@@ -1,21 +1,21 @@
 def add(a, b)
   puts "ADDING #{a} + #{b}"
-  return a + b
+  a + b
 end
 
 def subtract(a, b)
   puts "SUBTRACTING #{a} - #{b}"
-  return a - b
+  a - b
 end
 
 def multiply(a, b)
   puts "MULTIPLYING #{a} * #{b}"
-  return a * b
+  a * b
 end
 
 def divide(a, b)
   puts "DIVIDING #{a} / #{b}"
-  return a / b
+  a / b
 end
 
 
@@ -29,7 +29,6 @@ iq = divide(100, 2)
 puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
 
 
-# A puzzle for the extra credit, type it in anyway.
 puts "Here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
