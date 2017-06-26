2acc0a00e9b15a553f178d23da4ae4ffa6905390
Kurt Meyerhofer <k@kcmr.io>
2017-06-26 16:00:43 -0700

    fixes all errors

--- ex26.rb
+++ ex26.rb
@@ -1,9 +1,9 @@
-module Ex2
+module Ex25
 
   # This function will break up words for us.
   def Ex25.break_words(stuff)
     words = stuff.split(' ')
-    return word
+    return words
   end
 
   # Sorts the words.
@@ -20,30 +20,29 @@ module Ex2
   # Prints the last word after popping it off.
   def Ex25.print_last_word(words)
     word = words.pop
-    put word
+    puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
   def Ex25.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
-  ed
+  end
 
   # Prints the first and last words of the sentence.
   def Ex25.print_first_and_last(sentence)
     words = Ex25.break_words(sentence)
-    Ex25.print_first_word(word)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one.
   def Ex25.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
-
-
+end
 
 puts "Let's practice everything."
 puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
