97096da6e0667e29c061c1b1bc8fe7fc153172ea
Kurt Meyerhofer <k@kcmr.io>
2017-06-27 03:23:09 -0700

    adds functions

--- ex33.rb
+++ ex33.rb
@@ -1,16 +1,29 @@
-i = 0
-numbers = []
-
-while i < 6
+def loop(i, j)
+  numbers = []
+  while i < j
   puts "At the top i is #{i}"
   numbers.push(i)
 
   i += 1
   puts "Numbers now: ", numbers
   puts "At the bottom i is #{i}"
+
+  puts "The numbers: "
+
+  numbers.each {|num| puts num }
+  end
 end
 
-puts "The numbers: "
-
-# remember you can write this 2 other ways?
-numbers.each {|num| puts num }
+def question
+  puts "Enter a number between 1 and 10. Enter q to quit."
+  puts "> "
+  input = $stdin.gets.chomp.to_i
+  if input <= 10
+    loop(0, input)
+  elsif input == "q"
+  else
+    puts "Incorrect value."
+    question
+  end
+end
+question
